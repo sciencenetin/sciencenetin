55401e96efe71f3a8b2b6d210724c32e7c85c581
sciencenetin <79894485+sciencenetin@users.noreply.github.com>
2021-03-11 15:44:16 +0530

    Delete common.js

--- js/common.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-
-  var path = window.location.pathname.substring(1);
-  path = '/' + path.trim();
-  $('.scrollmenu>a[href="' + path + '"]').addClass('active');
-
-});
